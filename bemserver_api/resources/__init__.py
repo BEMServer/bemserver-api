1765efd0585441c70a0fae5e3405025f2920ed8e
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2021-06-02 10:39:28 +0200

    Reorder resources import
    Affects order in the spec visualization interface

--- bemserver_api/resources/__init__.py
+++ bemserver_api/resources/__init__.py
@@ -1,24 +1,24 @@
 """Resources initialization"""
 
-from . import timeseries
-from . import timeseries_data
-from . import events
 from . import users
 from . import campaigns
 from . import users_by_campaigns
+from . import timeseries
 from . import timeseries_by_campaigns
 from . import timeseries_by_campaigns_by_users
+from . import timeseries_data
+from . import events
 
 
 MODULES = (
-    timeseries,
-    timeseries_data,
-    events,
     users,
     campaigns,
     users_by_campaigns,
+    timeseries,
     timeseries_by_campaigns,
     timeseries_by_campaigns_by_users,
+    timeseries_data,
+    events,
 )
 
 
