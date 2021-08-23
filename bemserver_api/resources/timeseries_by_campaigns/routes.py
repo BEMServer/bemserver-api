fb6441593e47a072d7f69c47cf358acec0892b01
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2021-08-23 18:08:42 +0200

    Add permissions to timeseries by campaigns resource

--- bemserver_api/resources/timeseries_by_campaigns/routes.py
+++ bemserver_api/resources/timeseries_by_campaigns/routes.py
@@ -6,6 +6,7 @@ from bemserver_core.model import TimeseriesByCampaign
 
 from bemserver_api import Blueprint
 from bemserver_api.database import db
+from bemserver_api.resources.campaigns import blp as campaigns_blp
 
 from .schemas import (
     TimeseriesByCampaignSchema,
@@ -24,13 +25,15 @@ blp = Blueprint(
 @blp.route('/')
 class TimeseriesByCampaignViews(MethodView):
 
+    @blp.login_required(role=["admin"])
     @blp.etag
     @blp.arguments(TimeseriesByCampaignQueryArgsSchema, location='query')
     @blp.response(200, TimeseriesByCampaignSchema(many=True))
     def get(self, args):
         """List campaign x timeseries associations"""
-        return db.session.query(TimeseriesByCampaign).filter_by(**args)
+        return TimeseriesByCampaign.get(**args)
 
+    @blp.login_required(role=["admin"])
     @blp.etag
     @blp.arguments(TimeseriesByCampaignSchema)
     @blp.response(201, TimeseriesByCampaignSchema)
@@ -45,21 +48,52 @@ class TimeseriesByCampaignViews(MethodView):
 @blp.route('/<int:item_id>')
 class TimeseriesByCampaignByIdViews(MethodView):
 
+    @blp.login_required(role=["admin"])
     @blp.etag
     @blp.response(200, TimeseriesByCampaignSchema)
     def get(self, item_id):
         """Get campaign x timeseries association by ID"""
-        item = db.session.get(TimeseriesByCampaign, item_id)
+        item = TimeseriesByCampaign.get_by_id(item_id)
         if item is None:
             abort(404)
         return item
 
+    @blp.login_required(role=["admin"])
     @blp.response(204)
     @blp.catch_integrity_error
     def delete(self, item_id):
         """Delete a campaign x timeseries association"""
-        item = db.session.get(TimeseriesByCampaign, item_id)
+        item = TimeseriesByCampaign.get_by_id(item_id)
         if item is None:
             abort(404)
-        db.session.delete(item)
+        item.delete()
         db.session.commit()
+
+
+@campaigns_blp.route('/<int:campaign_id>/timeseriesbycampaigns/')
+class TimeseriesByCampaignForCampaignViews(MethodView):
+
+    @campaigns_blp.login_required(role=["admin", "user"])
+    @campaigns_blp.etag
+    @campaigns_blp.arguments(
+        TimeseriesByCampaignQueryArgsSchema(exclude=("campaign_id", )),
+        location='query'
+    )
+    @campaigns_blp.response(200, TimeseriesByCampaignSchema(many=True))
+    def get(self, args, campaign_id):
+        """List campaign x timeseries associations for campaign"""
+        return TimeseriesByCampaign.get(campaign_id=campaign_id, **args)
+
+
+@campaigns_blp.route('/<int:campaign_id>/timeseriesbycampaigns/<int:item_id>')
+class TimeseriesByCampaignForCampaignByIdViews(MethodView):
+
+    @campaigns_blp.login_required(role=["admin", "user"])
+    @campaigns_blp.etag
+    @campaigns_blp.response(200, TimeseriesByCampaignSchema)
+    def get(self, campaign_id, item_id):
+        """Get campaign x timeseries association by ID for campaign"""
+        item = TimeseriesByCampaign.get_by_id(item_id, campaign_id=campaign_id)
+        if item is None:
+            abort(404)
+        return item
--- tests/resources/test_timeseries_by_campaigns.py
+++ tests/resources/test_timeseries_by_campaigns.py
@@ -1,11 +1,16 @@
 """Timeseries by campaign routes tests"""
+import contextlib
+
 import pytest
+
+from tests.common import AuthTestConfig, AuthHeader
 
 
 DUMMY_ID = "69"
 
 TIMESERIES_URL = '/timeseries/'
 TIMESERIES_BY_CAMPAIGNS_URL = "/timeseriesbycampaigns/"
+CAMPAIGNS_URL = '/campaigns/'
 
 
 class TestTimeseriesByCampaignsApi:
@@ -131,3 +136,149 @@ class TestTimeseriesByCampaignsApi:
         # GET by id -> 404
         ret = client.get(f"{TIMESERIES_BY_CAMPAIGNS_URL}{tbc_1_id}")
         assert ret.status_code == 404
+
+    @pytest.mark.parametrize(
+        "app", (AuthTestConfig, ), indirect=True
+    )
+    @pytest.mark.parametrize(
+            'timeseries_data',
+            ({"nb_ts": 1, "nb_tsd": 0}, ),
+            indirect=True
+    )
+    def test_timeseries_by_campaigns_as_admin_api(
+        self, app, users, timeseries_data, campaigns
+    ):
+
+        ts_1_id, _, _, _ = timeseries_data[0]
+        campaign_1_id, _ = campaigns
+
+        creds = users["Chuck"]["creds"]
+
+        client = app.test_client()
+
+        with AuthHeader(creds):
+
+            # GET list
+            ret = client.get(TIMESERIES_BY_CAMPAIGNS_URL)
+            assert ret.status_code == 200
+            assert ret.json == []
+
+            # POST
+            tbc_1 = {"campaign_id": campaign_1_id, "timeseries_id": ts_1_id}
+            ret = client.post(TIMESERIES_BY_CAMPAIGNS_URL, json=tbc_1)
+            assert ret.status_code == 201
+            ret_val = ret.json
+            tbc_1_id = ret_val.pop("id")
+            tbc_1_etag = ret.headers["ETag"]
+
+            # GET list
+            ret = client.get(TIMESERIES_BY_CAMPAIGNS_URL)
+            assert ret.status_code == 200
+            ret_val = ret.json
+            assert len(ret_val) == 1
+            assert ret_val[0]["id"] == tbc_1_id
+
+            # GET by id
+            ret = client.get(f"{TIMESERIES_BY_CAMPAIGNS_URL}{tbc_1_id}")
+            assert ret.status_code == 200
+            assert ret.headers["ETag"] == tbc_1_etag
+
+            # DELETE
+            ret = client.delete(f"{TIMESERIES_BY_CAMPAIGNS_URL}{tbc_1_id}")
+            assert ret.status_code == 204
+
+    @pytest.mark.parametrize(
+        "app", (AuthTestConfig, ), indirect=True
+    )
+    @pytest.mark.parametrize("user", ("user", "anonym"))
+    def test_timeseries_by_campains_as_user_or_anonym_api(
+        self, app, user, users,
+        timeseries_data, campaigns, timeseries_by_campaigns
+    ):
+
+        ts_1_id, _, _, _ = timeseries_data[0]
+        tbc_1_id, _ = timeseries_by_campaigns
+        campaign_1_id, _ = campaigns
+
+        if user == "user":
+            creds = users["Active"]["creds"]
+            auth_context = AuthHeader(creds)
+            status_code = 403
+        else:
+            auth_context = contextlib.nullcontext()
+            status_code = 401
+
+        client = app.test_client()
+
+        with auth_context:
+
+            # GET list
+            ret = client.get(TIMESERIES_BY_CAMPAIGNS_URL)
+            assert ret.status_code == status_code
+
+            # POST
+            tbc = {"campaign_id": campaign_1_id, "timeseries_id": ts_1_id}
+            ret = client.post(TIMESERIES_BY_CAMPAIGNS_URL, json=tbc)
+            assert ret.status_code == status_code
+
+            # GET list
+            ret = client.get(TIMESERIES_BY_CAMPAIGNS_URL)
+            assert ret.status_code == status_code
+
+            # GET by id
+            ret = client.get(f"{TIMESERIES_BY_CAMPAIGNS_URL}{tbc_1_id}")
+            assert ret.status_code == status_code
+
+            # DELETE
+            ret = client.delete(f"{TIMESERIES_BY_CAMPAIGNS_URL}{tbc_1_id}")
+            assert ret.status_code == status_code
+
+
+class TestTimeseriesByCampaignForCampaignApi:
+
+    @pytest.mark.parametrize(
+        "app", (AuthTestConfig, ), indirect=True
+    )
+    @pytest.mark.parametrize("user", ("admin", "user", "anonym"))
+    @pytest.mark.usefixtures("timeseries_data")
+    @pytest.mark.usefixtures("users_by_campaigns")
+    def test_timeseries_by_campaign_for_campaign_api(
+        self, app, user, users, campaigns, timeseries_by_campaigns
+    ):
+        campaign_1_id, _ = campaigns
+        tbc_1_id, _ = timeseries_by_campaigns
+
+        if user == "admin":
+            creds = users["Chuck"]["creds"]
+            auth_context = AuthHeader(creds)
+        elif user == "user":
+            creds = users["Active"]["creds"]
+            auth_context = AuthHeader(creds)
+        else:
+            auth_context = contextlib.nullcontext()
+
+        client = app.test_client()
+
+        with auth_context:
+
+            # GET list
+            ret = client.get(
+                f"{CAMPAIGNS_URL}{campaign_1_id}/timeseriesbycampaigns/")
+            if user == "anonym":
+                assert ret.status_code == 401
+            else:
+                assert ret.status_code == 200
+                ret_val = ret.json
+                assert len(ret_val) == 1
+                assert ret_val[0]["id"] == tbc_1_id
+
+            # GET by id
+            ret = client.get(
+                f"{CAMPAIGNS_URL}{campaign_1_id}/"
+                f"timeseriesbycampaigns/{tbc_1_id}"
+            )
+            if user == "anonym":
+                assert ret.status_code == 401
+            else:
+                assert ret.status_code == 200
+                assert ret.json["id"] == tbc_1_id
