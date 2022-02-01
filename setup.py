0d8d4639325d1e9412ffc0b7dbd8e5e8075645b5
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2022-02-01 15:41:32 +0100

    Support Python 3.10

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         (
             "License :: OSI Approved :: "
             "GNU Affero General Public License v3 or later (AGPLv3+)"
