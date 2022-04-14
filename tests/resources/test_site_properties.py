b72b82b7b176eb490aeedf9984eedba61629d97d
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2022-04-14 16:09:31 +0200

    Dump structural_element_property with site_property

--- tests/resources/test_site_properties.py
+++ tests/resources/test_site_properties.py
@@ -32,6 +32,8 @@ class TestSitePropertiesApi:
             ret_val = ret.json
             site_p_1_id = ret_val.pop("id")
             site_p_1_etag = ret.headers["ETag"]
+            sep = ret_val.pop("structural_element_property")
+            assert sep == {"name": "Surface"}
             assert ret_val == site_p_1
 
             # POST violating unique constraint
@@ -51,6 +53,7 @@ class TestSitePropertiesApi:
             assert ret.headers["ETag"] == site_p_1_etag
             ret_val = ret.json
             ret_val.pop("id")
+            ret_val.pop("structural_element_property")
             assert ret_val == site_p_1
 
             # POST sep 2
