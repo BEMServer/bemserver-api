25a9b1bbc6ed24887795568ca6699f1aeb8f8222
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2022-04-14 16:13:47 +0200

    Dump structural_element_property with storey_property

--- bemserver_api/resources/storey_properties/schemas.py
+++ bemserver_api/resources/storey_properties/schemas.py
@@ -6,6 +6,7 @@ import marshmallow_sqlalchemy as msa
 from bemserver_core.model import StoreyProperty
 
 from bemserver_api import AutoSchema, Schema
+from ..structural_element_properties.schemas import StructuralElementPropertySchema
 
 
 class StoreyPropertySchema(AutoSchema):
@@ -13,6 +14,10 @@ class StoreyPropertySchema(AutoSchema):
         table = StoreyProperty.__table__
 
     id = msa.auto_field(dump_only=True)
+    structural_element_property = ma.fields.Nested(
+        StructuralElementPropertySchema(exclude=("id",)),
+        dump_only=True,
+    )
 
 
 class StoreyPropertyQueryArgsSchema(Schema):
