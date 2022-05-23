6cb31ee1efafcaa5bde366e17f18ec333d6d2db2
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2022-05-23 13:48:57 +0200

    Bump bemserver-core revision

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         (
             # https://github.com/jazzband/pip-tools/issues/1359
             "bemserver-core @ "
-            "https://github.com/BEMServer/bemserver-core/archive/a839928.tar.gz"
+            "https://github.com/BEMServer/bemserver-core/archive/a1b1304.tar.gz"
         ),
     ],
     packages=find_packages(exclude=["tests*"]),
