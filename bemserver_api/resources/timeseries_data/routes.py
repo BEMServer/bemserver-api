993c72f7feaddf181c04891a4aba3411c3e20784
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2022-06-07 15:52:20 +0200

    Split TimeseriesData resources in two Blueprints

--- bemserver_api/resources/timeseries_data/routes.py
+++ bemserver_api/resources/timeseries_data/routes.py
@@ -53,6 +53,14 @@ blp = Blueprint(
 )
 
 
+blp4c = Blueprint(
+    "TimeseriesDataForCampaign",
+    __name__,
+    url_prefix="/timeseries_data/campaign/<int:campaign_id>",
+    description="Operations on timeseries data for a given campaign",
+)
+
+
 @blp.route("/", methods=("GET",))
 @blp.login_required
 @blp.arguments(TimeseriesDataGetByIDQueryArgsSchema, location="query")
@@ -187,17 +195,17 @@ def delete(args):
         abort(422, message=str(exc))
 
 
-@blp.route("/campaign/<int:campaign_id>/", methods=("GET",))
-@blp.login_required
-@blp.arguments(TimeseriesDataGetByNameQueryArgsSchema, location="query")
-@blp.response(
+@blp4c.route("/", methods=("GET",))
+@blp4c.login_required
+@blp4c.arguments(TimeseriesDataGetByNameQueryArgsSchema, location="query")
+@blp4c.response(
     200,
     {"format": "binary", "type": "string"},
     content_type="application/csv",
     example=EXAMPLE_CSV_IN_OUT_FILE_TS_NAME,
 )
 def get_csv_for_campaign(args, campaign_id):
-    """Get timeseries data as CSV file
+    """Get timeseries data as CSV file for a given campaign
 
     Returns a CSV file where the first column is the timestamp as timezone aware
     datetime and each other column is a timeseries data. Column headers are
@@ -233,17 +241,17 @@ def get_csv_for_campaign(args, campaign_id):
     return response
 
 
-@blp.route("/campaign/<int:campaign_id>/aggregate", methods=("GET",))
-@blp.login_required
-@blp.arguments(TimeseriesDataGetByNameAggregateQueryArgsSchema, location="query")
-@blp.response(
+@blp4c.route("/aggregate", methods=("GET",))
+@blp4c.login_required
+@blp4c.arguments(TimeseriesDataGetByNameAggregateQueryArgsSchema, location="query")
+@blp4c.response(
     200,
     {"format": "binary", "type": "string"},
     content_type="application/csv",
     example=EXAMPLE_CSV_IN_OUT_FILE_TS_NAME,
 )
 def get_aggregate_csv_for_campaign(args, campaign_id):
-    """Get aggregated timeseries data as CSV file
+    """Get aggregated timeseries data as CSV file for a given campaign
 
     Returns a CSV file where the first column is the timestamp as timezone aware
     datetime and each other column is a timeseries data. Column headers are
@@ -283,13 +291,13 @@ def get_aggregate_csv_for_campaign(args, campaign_id):
     return response
 
 
-@blp.route("/campaign/<int:campaign_id>/", methods=("POST",))
-@blp.login_required
-@blp.arguments(TimeseriesDataPostFileSchema, location="files")
-@blp.arguments(TimeseriesDataPostQueryArgsSchema, location="query")
-@blp.response(201)
+@blp4c.route("/", methods=("POST",))
+@blp4c.login_required
+@blp4c.arguments(TimeseriesDataPostFileSchema, location="files")
+@blp4c.arguments(TimeseriesDataPostQueryArgsSchema, location="query")
+@blp4c.response(201)
 def post_csv_for_campaign(files, args, campaign_id):
-    """Post timeseries data as CSV file
+    """Post timeseries data as CSV file for a given campaign
 
     Loads a CSV file where the first column is the timestamp as timezone aware
     datetime and each other column is a timeseries data. Column headers are
@@ -316,12 +324,12 @@ def post_csv_for_campaign(files, args, campaign_id):
             abort(422, message=f"Invalid CSV file content: {exc}")
 
 
-@blp.route("/campaign/<int:campaign_id>/", methods=("DELETE",))
-@blp.login_required
-@blp.arguments(TimeseriesDataGetByNameQueryArgsSchema, location="query")
-@blp.response(204)
+@blp4c.route("/", methods=("DELETE",))
+@blp4c.login_required
+@blp4c.arguments(TimeseriesDataGetByNameQueryArgsSchema, location="query")
+@blp4c.response(204)
 def delete_for_campaign(args, campaign_id):
-    """Delete timeseries data"""
+    """Delete timeseries data for a given campaign"""
     campaign = Campaign.get_by_id(campaign_id)
     if campaign is None:
         abort(404)
