6c5061946d91d30d668f6d18cdd9636355f8cf0e
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2023-02-27 15:12:30 +0100

    Add UnitSymbol field

--- tests/extensions/test_ma_fields.py
+++ tests/extensions/test_ma_fields.py
@@ -2,7 +2,7 @@ import marshmallow as ma
 
 import pytest
 
-from bemserver_api.extensions.ma_fields import Timezone
+from bemserver_api.extensions.ma_fields import Timezone, UnitSymbol
 
 
 class TestMaFields:
@@ -12,3 +12,16 @@ class TestMaFields:
         assert field.deserialize("Europe/Paris") == "Europe/Paris"
         with pytest.raises(ma.ValidationError):
             field.deserialize("Dordogne/Boulazac")
+
+    def test_ma_fields_unit_symbol(self):
+        field = UnitSymbol()
+        assert field.deserialize("") == ""
+        assert field.deserialize("m") == "m"
+        assert field.deserialize("meter") == "meter"
+        assert field.deserialize("kWh") == "kWh"
+        assert field.deserialize("m/s") == "m/s"
+        assert field.deserialize("m3/m3") == "m3/m3"
+        with pytest.raises(ma.ValidationError):
+            field.deserialize("wh")
+        with pytest.raises(ma.ValidationError):
+            field.deserialize("dummy")
