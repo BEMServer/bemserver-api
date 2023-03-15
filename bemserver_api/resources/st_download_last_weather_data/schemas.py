8a9aa066da589d1d99a4e452cd64e4f1bdc21ebd
Colas NAUDI <colasnaudi@gmail.com>
2023-03-15 15:20:55 +0100

    Add ST_weather_data schemas file not finished

--- bemserver_api/resources/st_download_last_weather_data/schemas.py
+++ bemserver_api/resources/st_download_last_weather_data/schemas.py
@@ -7,18 +7,24 @@ from bemserver_core.scheduled_tasks import ST_DownloadLastWeatherData
 from bemserver_api import AutoSchema, Schema, SortField
 
 class ST_DownloadLastWeatherDataSchema(AutoSchema):
-    pass
+    class Meta(AutoSchema.Meta):
+        model = ST_DownloadLastWeatherData
+
+    id = msa.auto_field(dump_only=True)
+    is_enabled = msa.auto_field(metadata={"default": True})
 
 
 class ST_DownloadLastWeatherDataPutSchema(Schema):
-    pass
+    is_enabled = ma.fields.Boolean()
+
+
+class ST_DownloadLastWeatherDataFullSchema(Schema):
+    id = ma.fields.Integer(dump_only=True)
+    site_id = ma.fields.Integer(dump_only=True)
+    is_enabled = ma.fields.Boolean(dump_only=True)
 
 
 class ST_DownloadLastWeatherDataQueryArgsSchema(Schema):
-    pass
-
-
-class ST_DownloadLastWeatherDataFullSchema(AutoSchema):
     pass
 
 
