3582378045773093657f45c5db84ecf07a53fcee
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2023-05-05 18:31:32 +0200

    Require bemserver-core>=0.15.0

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         "flask_smorest>=0.41.0,<0.42",
         "apispec>=6.1.0,<7.0",
         "flask-httpauth>=4.7.0,<5.0",
-        "bemserver-core>=0.14.0,<0.15",
+        "bemserver-core>=0.15.0,<0.16",
     ],
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
