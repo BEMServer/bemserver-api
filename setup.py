f666e79aee095d26a19b8a271ec1c4c1b23d7284
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2023-06-09 17:07:34 +0200

    Bump version: 0.20.1 → 0.21.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.rst", encoding="utf-8") as f:
 
 setup(
     name="bemserver-api",
-    version="0.20.1",
+    version="0.21.0",
     description="BEMServer API",
     long_description=long_description,
     url="https://github.com/BEMServer/bemserver-api",
