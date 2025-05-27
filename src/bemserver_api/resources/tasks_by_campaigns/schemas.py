8eb27d363aec16c12b6406d8345dbff149662514
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2025-05-27 16:18:50 +0200

    TaskByCampaign: add/remove Scheduled suffix

--- src/bemserver_api/resources/tasks_by_campaigns/schemas.py
+++ src/bemserver_api/resources/tasks_by_campaigns/schemas.py
@@ -9,7 +9,7 @@ from bemserver_core.time_utils import PeriodEnum
 from bemserver_api import AutoSchema, Schema
 
 
-class TaskByCampaignSchema(AutoSchema):
+class TaskByCampaignBaseSchema(AutoSchema):
     class Meta(AutoSchema.Meta):
         model = TaskByCampaign
 
@@ -19,7 +19,19 @@ class TaskByCampaignSchema(AutoSchema):
     offset_unit = ma.fields.Enum(PeriodEnum)
 
 
-class TaskByCampaignPutSchema(TaskByCampaignSchema):
+class TaskByCampaignSchema(TaskByCampaignBaseSchema):
+    @ma.post_load
+    def add_scheduled_suffix(self, item, many, **kwargs):
+        item["task_name"] = f"{item['task_name']}Scheduled"
+        return item
+
+    @ma.post_dump
+    def remove_scheduled_suffix(self, data, many, **kwargs):
+        data["task_name"] = data["task_name"].removesuffix("Scheduled")
+        return data
+
+
+class TaskByCampaignPutSchema(TaskByCampaignBaseSchema):
     class Meta(TaskByCampaignSchema.Meta):
         exclude = ("campaign_id", "task_name")
 
